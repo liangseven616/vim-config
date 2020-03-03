28e5eaffb6bed7a581ff42a4807ae4569537d96f
liangseven616 <34889512+liangseven616@users.noreply.github.com>
2020-03-03 09:25:35 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,29 +1,32 @@
 #!/usr/bin/env bash
-
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo cp -r .vimrc ~/.vimrc
 sudo rm -rf ~/.vim
 sudo cp -r .vim ~/.vim
 sudo curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
 
-mkdir ~/vim-config
-cd ~/vim_config
-mkdir ctags && cd ctags
-git clone https://github.com/universal-ctags/ctags.git
-cd ctags
-./autogen.sh
-./configure --prefix=/opt/software/universal-ctags 
-make -j4
-sudo make install
-
-cd .. && mkdir gtags && cd gtags
-wget http://tamacom.com/global/global-6.6.4.tar.gz
-tar -xf global-6.6.4.tar.gz
-cd global-6.6.4
-./configure --with-sqlite3   #gtags可以使用Sqlite3作为数据库，在编译时需要加这个参数
-make -j4     #四线程并行编译
-make check
-sudo make install
-sudo make install check
+if [ ! -d "/ctags" ]; then
+  mkdir ctags && cd ctags
+  git clone https://github.com/universal-ctags/ctags.git
+  cd ctags
+  ./autogen.sh
+  ./configure --prefix=/opt/software/universal-ctags 
+  make -j4
+  sudo make install
+  cd ..
+fi
+if [ ! -d "/gtags" ]; then
+  mkdir gtags && cd gtags
+  wget http://tamacom.com/global/global-6.6.4.tar.gz
+  tar -xf global-6.6.4.tar.gz
+  cd global-6.6.4
+  ./configure --with-sqlite3   #gtags可以使用Sqlite3作为数据库，在编译时需要加这个参数
+  make -j4     #四线程并行编译
+  make check
+  sudo make install
+  sudo make install check
+  cd ..
+fi
 
 source /etc/os-release
 case $ID in
@@ -31,12 +34,12 @@ debian|ubuntu|devuan)
 	echo "Linux : $ID"
 	sudo apt install nodejs
 	sudo apt install npm 
-	sudo apt install snapd
 	sudo apt install yarn
+	sudo apt install clangd
 	;;
 centos|fedora|rhel)
 esac
 curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
-sudo snap install ccls --classic
 sudo npm i -g bash-language-server
 sudo vim -c "PlugInstall" -c "q" -c "q"
+sudo vim -c "CocInstall coc-clangd" -c "q" -c "q"
